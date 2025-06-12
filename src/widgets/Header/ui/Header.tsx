5926b597ad089c67bca19b52617ecf0242a53568
Alexander Leontyev <orientstar96@gmail.com>
2025-06-12 21:11:32 +0500

    fix: header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,16 +1,17 @@
 import { useStore } from '@store/index';
 import { LogoutBtn } from '@features/logoutBtn';
+import { selectIsAuth } from '@store/selectors';
 import '../style.pcss';
 
 export const Header = () => {
-  const isAuth = useStore((state) => state.isAuth);
+  const isAuthenticated = useStore(selectIsAuth);
 
   return (
     <header className="header">
       <div className="container">
         <div className="header__inner">
           <p className="header__title">React calendar</p>
-          {isAuth && <LogoutBtn label="Выйти" />}
+          {isAuthenticated && <LogoutBtn label="Выйти" />}
         </div>
       </div>
     </header>
